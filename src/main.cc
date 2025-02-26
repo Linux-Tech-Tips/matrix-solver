7dbb53ff7b68e981fadfc9749cd49802b6933db4
Linux-Tech-Tips <disposablemail404.2@gmail.com>
2025-02-26 04:30:27 +0000

    Added main.cc basic app source code

--- src/main.cc
+++ src/main.cc
@@ -1,13 +1,141 @@
-/** Test Main file, meant to test functionality */
+/** App Main file for the matrix solver */
 
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+
+#include "Matrix/Matrix.hh"
+#include "Matrix/MatrixUtil.hh"
+#include "Rational/Rational.hh"
 
 #include "Rational/RationalTest.hh"
 #include "Matrix/MatrixTest.hh"
 #include "Matrix/MatrixUtilTest.hh"
 
-int main(void) {
+/** Asks the user to enter a Matrix and saves it into m */
+void enterMatrix(Matrix<Rational>& m);
 
+/** Asks the user for a Matrix, reduces to REF */
+void ref(void);
+
+/** Asks the user for a Matrix, reduces to RREF */
+void rref(void);
+
+/** Asks the user for a Matrix, prints both REF and RREF */
+void forms(void);
+
+/** Runs Rational and Matrix TCs */
+void test(void);
+
+/** Prints out help information */
+void help(void);
+
+int main(int argc, char const ** argv) {
+
+    /* Display help info immediately if help asked */
+    for(int i = 0; i < argc; ++i) {
+	if(std::strcmp(argv[i], "-h") == 0 || std::strcmp(argv[i], "--help") == 0) {
+	    help();
+	    return 0;
+	}
+    }
+
+    /* Intro Text */
+    std::puts("=== C++ Matrix (Gauss-Jordan Elimination) Solver ===");
+    std::puts("Enter command (ref/rref/forms/test/help/exit)");
+
+    /* Scanning command input from the user until exit */
+    bool run = true;
+    while(run) {
+	std::string userIn;
+	std::printf("> ");
+	std::getline(std::cin, userIn);
+	if(userIn == "ref") {
+	    ref();
+	} else if(userIn == "rref") {
+	    rref();
+	} else if(userIn == "forms") {
+	    forms();
+	} else if(userIn == "test") {
+	    test();
+	} else if(userIn == "help") {
+	    help();
+	} else if(userIn == "exit") {
+	    std::puts("Program Exit");
+	    run = false;
+	} else {
+	    std::printf("Error: Unrecognized command: %s\n", userIn.c_str());
+	}
+    }
+
+    return 0;
+}
+
+
+/* --- Function Implementations --- */
+
+void enterMatrix(Matrix<Rational>& m) {
+    std::puts("Enter Matrix (space separated Rational numbers, new line makes a new row, type DONE on a new line to stop):");
+    /* Going through user input row by row and saving to get full Matrix */
+    std::string matrix;
+    while(true) {
+	std::string row;
+	std::getline(std::cin, row);
+	if(row == "DONE" || row == "done")
+	    break;
+	matrix += row + "\n";
+    }
+    /* Processing user input to resize and populate Matrix */
+    size_t rows = static_cast<size_t>(std::count(matrix.begin(), matrix.end(), '\n'));
+    size_t cols = static_cast<size_t>(std::count(matrix.begin(), std::find(matrix.begin(), matrix.end(), '\n'), ' ')) + 1;
+    m.resize(cols, rows);
+    /* Go through user input to populate Matrix with the correct values */
+    size_t col = 0, row = 0;
+    std::string number;
+    for(char c : matrix) {
+	if(c == '\n' || c == ' ') {
+	    m.at(col, row) = number;
+	    number = "";
+	    if(c == '\n') {
+		col = 0;
+		++row;
+	    } else {
+		++col;
+	    }
+	} else {
+	    number += c;
+	}
+    }
+}
+
+void ref(void) {
+    Matrix<Rational> m;
+    enterMatrix(m);
+    std::printf("Entered Matrix:\n%s\n", m.print([](Rational r) { return r.toString(); }).c_str());
+    MatrixReduce::toREF(m);
+    std::printf("Matrix in REF:\n%s\n", m.print([](Rational r) { return r.toString(); }).c_str());
+}
+
+void rref(void) {
+    Matrix<Rational> m;
+    enterMatrix(m);
+    std::printf("Entered Matrix:\n%s\n", m.print([](Rational r) { return r.toString(); }).c_str());
+    MatrixReduce::toRREF(m);
+    std::printf("Matrix in RREF:\n%s\n", m.print([](Rational r) { return r.toString(); }).c_str());
+}
+
+void forms(void) {
+    Matrix<Rational> m;
+    enterMatrix(m);
+    std::printf("Entered Matrix:\n%s\n", m.print([](Rational r) { return r.toString(); }).c_str());
+    MatrixReduce::toREF(m);
+    std::printf("Matrix in REF:\n%s\n", m.print([](Rational r) { return r.toString(); }).c_str());
+    MatrixReduce::REFtoRREF(m);
+    std::printf("Matrix in RREF:\n%s\n", m.print([](Rational r) { return r.toString(); }).c_str());
+}
+
+void test(void) {
     /* Calling all Rational test cases */
     rationalTest();
 
@@ -16,6 +144,22 @@ int main(void) {
 
     /* Calling all MatrixUtil test cases */
     matrixUtilTest();
+}
 
-    return 0;
+void help(void) {
+    std::puts("HELP INFORMATION:\n"
+	      "This program reduces entered matrices to REF or RREF based on user preference.\n"
+	      " -> Available commands:\n"
+	      "   -> ref .... reduce to REF\n"
+	      "   -> rref ... reduce to RREF\n"
+	      "   -> forms .. reduce and show both REF and RREF\n"
+	      "   -> test ... run program utility test cases\n"
+	      "   -> help ... display this help info\n"
+	      "   -> exit ... quit the program\n"
+	      " -> Entering Matrices:\n"
+	      "   -> enter rational numbers in format [-]<A>[/<B>]\n"
+	      "      (A is the numerator, B the denominator, anything in '[]' is optional, anything in '<>' is mandatory, exclude the brackets when entering numbers)\n"
+	      "   -> separate each new number in the same row with a single space, end row with a line break\n"
+	      "   -> end Matrix input with EOF/Ctrl+d/Ctrl+z"
+    );
 }
